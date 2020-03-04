0d5a16254b90ce94638271f708917efd4c490c43
duddel <duddel@users.noreply.github.com>
2020-03-04 21:50:32 +0000

    Deploying to gh-pages from  - eb71fe0ff4c6bf779877838e51b3a4b09947438a 🚀

--- examples/framework.js
+++ examples/framework.js
@@ -194,7 +194,7 @@ var Module = typeof Module !== 'undefined' ? Module : {};
     }
   
    }
-   loadPackage({"remote_package_size": 523, "files": [{"filename": "/assets/simple.vert", "end": 130, "start": 0, "audio": 0}, {"filename": "/assets/simple.es.frag", "end": 274, "start": 130, "audio": 0}, {"filename": "/assets/simple.es.vert", "end": 407, "start": 274, "audio": 0}, {"filename": "/assets/simple.frag", "end": 523, "start": 407, "audio": 0}], "package_uuid": "95552d7c-d2a7-43ba-9105-21360ea54abb"});
+   loadPackage({"remote_package_size": 523, "files": [{"audio": 0, "start": 0, "end": 130, "filename": "/assets/simple.vert"}, {"audio": 0, "start": 130, "end": 274, "filename": "/assets/simple.es.frag"}, {"audio": 0, "start": 274, "end": 407, "filename": "/assets/simple.es.vert"}, {"audio": 0, "start": 407, "end": 523, "filename": "/assets/simple.frag"}], "package_uuid": "593fe7b8-b945-4ef4-97f7-93b2fa5a300c"});
   
   })();
   
