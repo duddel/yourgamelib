ca08a45654a298b84a09a6eacddcde534e7a9e64
duddel <duddel@users.noreply.github.com>
2020-06-18 17:12:51 +0000

    Deploying to gh-pages from  @ 026a2cbcf88fa0a578ba708c22a1c7c0ff75d410 🚀

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,24 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "dimsu",
-  1: "m",
-  2: "m",
-  3: "disu"
+  0: "m",
+  1: "m"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "namespaces",
-  2: "files",
-  3: "functions"
+  1: "files"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Namespaces",
-  2: "Files",
-  3: "Functions"
+  1: "Files"
 };
 
