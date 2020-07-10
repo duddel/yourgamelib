ffda1c9890f226d518dbf21200745969af388135
duddel <duddel@users.noreply.github.com>
2020-07-10 23:40:56 +0200

    extend Android NativeActivity with Kotlin

---
+++ template/android/app/src/main/java/MainActivity.kt
@@ -0,0 +1,10 @@
+package yourgame.project
+
+import android.app.NativeActivity
+import android.os.Bundle
+
+class MainActivity : NativeActivity() {
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
