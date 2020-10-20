6e4db72ce7aa9e30787302bd8953761cd746bb0c
duddel <duddel@users.noreply.github.com>
2020-10-20 21:08:22 +0200

    add yg namespace alias to template (convenience)

--- template/mygame.cpp
+++ template/mygame.cpp
@@ -1,23 +1,25 @@
 #include "yourgame/yourgame.h"
 #include "yourgame/gl_include.h"
 
+namespace yg = yourgame; // convenience
+
 namespace mygame
 {
-    void init(const yourgame::context &ctx)
+    void init(const yg::context &ctx)
     {
         glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
     }
 
-    void tick(const yourgame::context &ctx)
+    void tick(const yg::context &ctx)
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        if (yourgame::getInputi(yourgame::InputSource::YOURGAME_KEY_ESCAPE))
+        if (yg::getInputi(yg::InputSource::YOURGAME_KEY_ESCAPE))
         {
-            yourgame::notifyShutdown();
+            yg::notifyShutdown();
         }
     }
 
-    void shutdown(const yourgame::context &ctx) {}
+    void shutdown(const yg::context &ctx) {}
 
 } // namespace mygame
