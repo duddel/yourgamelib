e56bb67c77d1945e867c6c45def61a7d2c0f4d91
duddel <duddel@users.noreply.github.com>
2020-11-07 13:29:50 +0100

    remove timer in favor of std::chrono

--- src/android/yourgame.cpp
+++ src/android/yourgame.cpp
@@ -17,6 +17,7 @@ freely, subject to the following restrictions:
    misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
 */
+#include <chrono>
 #include <android/native_window.h>
 #include <android_native_app_glue.h>
 #include <android/log.h>
@@ -25,7 +26,6 @@ freely, subject to the following restrictions:
 #include "yourgame/yourgame.h"
 #include "yourgame/gl_include.h"
 #include "yourgame_internal/yourgame_port.h"
-#include "yourgame_internal/timer.h"
 #include "yourgame_internal/mygame_external.h"
 
 #ifdef YOURGAME_EXTPROJ_imgui
@@ -39,295 +39,298 @@ INITIALIZE_EASYLOGGINGPP
 namespace yourgame
 {
 
-class elAndroidDispatcher : public el::LogDispatchCallback
-{
-protected:
-    void handle(const el::LogDispatchData *data) noexcept override
+    class elAndroidDispatcher : public el::LogDispatchCallback
     {
-        auto logMsg = data->logMessage();
-        switch (logMsg->level())
+    protected:
+        void handle(const el::LogDispatchData *data) noexcept override
         {
-        case el::Level::Debug:
-            __android_log_print(ANDROID_LOG_DEBUG, "yourgame_app", "%s", logMsg->message().c_str());
-            break;
-        case el::Level::Info:
-            __android_log_print(ANDROID_LOG_INFO, "yourgame_app", "%s", logMsg->message().c_str());
-            break;
-        case el::Level::Warning:
-            __android_log_print(ANDROID_LOG_WARN, "yourgame_app", "%s", logMsg->message().c_str());
-            break;
-        case el::Level::Error:
-            __android_log_print(ANDROID_LOG_ERROR, "yourgame_app", "%s", logMsg->message().c_str());
-            break;
+            auto logMsg = data->logMessage();
+            switch (logMsg->level())
+            {
+            case el::Level::Debug:
+                __android_log_print(ANDROID_LOG_DEBUG, "yourgame_app", "%s", logMsg->message().c_str());
+                break;
+            case el::Level::Info:
+                __android_log_print(ANDROID_LOG_INFO, "yourgame_app", "%s", logMsg->message().c_str());
+                break;
+            case el::Level::Warning:
+                __android_log_print(ANDROID_LOG_WARN, "yourgame_app", "%s", logMsg->message().c_str());
+                break;
+            case el::Level::Error:
+                __android_log_print(ANDROID_LOG_ERROR, "yourgame_app", "%s", logMsg->message().c_str());
+                break;
+            }
         }
-    }
-};
+    };
 
-namespace
-{
-yourgame::context _context;
-yourgame_internal::Timer _timer(0U);
-bool _initialized = false;
-
-EGLDisplay _display = EGL_NO_DISPLAY;
-EGLSurface _surface = EGL_NO_SURFACE;
-EGLContext _eglContext = EGL_NO_CONTEXT;
-ANativeWindow *_win = NULL;
-struct android_app *_app = NULL;
-
-/*
-todo: this is called every frame, but actually only required
-if the screen size changed. however, the app command
-APP_CMD_WINDOW_RESIZED is not invoked reliable.
-*/
-void updateContextWinSize()
-{
-    EGLint width, height;
-    eglQuerySurface(_display, _surface, EGL_WIDTH, &width);
-    eglQuerySurface(_display, _surface, EGL_HEIGHT, &height);
-    _context.winWidth = width;
-    _context.winHeight = height;
-    _context.winAspectRatio = (float)width / (float)height;
-}
-} // namespace
-
-// API
-const yourgame::context &getCtx()
-{
-    return _context;
-}
-
-// do nothing. we do not exit the app manually
-void notifyShutdown() {}
-
-int sendCmdToEnv(int cmdId, int data0, int data1, int data2)
-{
-    // the whole procedure (get environment, etc.) is done every time to
-    // not risk inconsistencies with global references to JNI objects.
-
-    JavaVM* jVM = _app->activity->vm;
-    JNIEnv* jEnv = NULL;
-
-    // todo: clearify JNI version to request
-    jint jniRet = jVM->GetEnv((void**)&jEnv, JNI_VERSION_1_6);
-    if(jniRet == JNI_ERR)
+    namespace
     {
-        return -1;
+        yourgame::context _context;
+        std::chrono::steady_clock::time_point lastNowTime;
+        bool _initialized = false;
+
+        EGLDisplay _display = EGL_NO_DISPLAY;
+        EGLSurface _surface = EGL_NO_SURFACE;
+        EGLContext _eglContext = EGL_NO_CONTEXT;
+        ANativeWindow *_win = NULL;
+        struct android_app *_app = NULL;
+
+        /* todo: this is called every frame, but actually only required
+        if the screen size changed. however, the app command
+        APP_CMD_WINDOW_RESIZED is not invoked reliable.*/
+        void updateContextWinSize()
+        {
+            EGLint width, height;
+            eglQuerySurface(_display, _surface, EGL_WIDTH, &width);
+            eglQuerySurface(_display, _surface, EGL_HEIGHT, &height);
+            _context.winWidth = width;
+            _context.winHeight = height;
+            _context.winAspectRatio = (float)width / (float)height;
+        }
+    } // namespace
+
+    // API
+    const yourgame::context &getCtx()
+    {
+        return _context;
     }
 
-    jniRet = jVM->AttachCurrentThread(&jEnv, NULL);
-    if(jniRet != JNI_OK)
+    // do nothing. we do not exit the app manually
+    void notifyShutdown() {}
+
+    int sendCmdToEnv(int cmdId, int data0, int data1, int data2)
     {
-        return -2;
+        // the whole procedure (get environment, etc.) is done every time to
+        // not risk inconsistencies with global references to JNI objects.
+
+        JavaVM *jVM = _app->activity->vm;
+        JNIEnv *jEnv = NULL;
+
+        // todo: clearify JNI version to request
+        jint jniRet = jVM->GetEnv((void **)&jEnv, JNI_VERSION_1_6);
+        if (jniRet == JNI_ERR)
+        {
+            return -1;
+        }
+
+        jniRet = jVM->AttachCurrentThread(&jEnv, NULL);
+        if (jniRet != JNI_OK)
+        {
+            return -2;
+        }
+
+        jclass natActClazz = jEnv->GetObjectClass(_app->activity->clazz);
+        if (natActClazz == NULL)
+        {
+            return -3;
+        }
+
+        // get the "method ID"
+        jmethodID methodID = jEnv->GetMethodID(natActClazz, "receiveCmd", "(IIII)I");
+        if (methodID == NULL)
+        {
+            return -4;
+        }
+
+        // call the actual method
+        jint methodRet = jEnv->CallIntMethod(_app->activity->clazz, methodID, cmdId, data0, data1, data2);
+        if (methodRet != 0)
+        {
+            return -10;
+        }
+
+        jniRet = jVM->DetachCurrentThread();
+        if (jniRet != JNI_OK)
+        {
+            return -5;
+        }
+
+        return 0;
     }
 
-    jclass natActClazz = jEnv->GetObjectClass(_app->activity->clazz);
-    if(natActClazz == NULL)
+    // INTERNAL API
+    bool isInitialized()
     {
-        return -3;
+        return _initialized;
     }
 
-    // get the "method ID"
-    jmethodID methodID = jEnv->GetMethodID(natActClazz, "receiveCmd", "(IIII)I");
-    if(methodID == NULL)
+    void init(struct android_app *app)
     {
-        return -4;
-    }
+        if (_initialized)
+        {
+            return;
+        }
 
-    // call the actual method
-    jint methodRet = jEnv->CallIntMethod(_app->activity->clazz, methodID, cmdId, data0, data1, data2);
-    if(methodRet != 0)
-    {
-        return -10;
-    }
+        _win = app->window;
+        _app = app;
 
-    jniRet = jVM->DetachCurrentThread();
-    if(jniRet != JNI_OK)
-    {
-        return -5;
-    }
+        initInput(app);
+        initFileIO(app);
 
-    return 0;
-}
+        // initialize logging
+        _context.logger = el::Loggers::getLogger("default");
+        el::Helpers::installLogDispatchCallback<elAndroidDispatcher>("AndroidDispatcher");
+        el::Helpers::uninstallLogDispatchCallback<el::base::DefaultLogDispatchCallback>("DefaultLogDispatchCallback");
+        elAndroidDispatcher *dispatcher = el::Helpers::logDispatchCallback<elAndroidDispatcher>("AndroidDispatcher");
+        dispatcher->setEnabled(true);
 
-// INTERNAL API
-bool isInitialized()
-{
-    return _initialized;
-}
+        // initialize timing
+        lastNowTime = std::chrono::steady_clock::now();
+        double clockPeriod = (double)std::chrono::steady_clock::period::num /
+                             (double)std::chrono::steady_clock::period::den;
+        yourgame::logi("steady_clock precision: %vs (%vns)", clockPeriod, clockPeriod * 1.0e+9);
 
-void init(struct android_app *app)
-{
-    if (_initialized)
-    {
-        return;
-    }
+        // initialize EGL
+        _display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+        if (_display == EGL_NO_DISPLAY)
+        {
+            yourgame::loge("eglGetDisplay(EGL_DEFAULT_DISPLAY) returned EGL_NO_DISPLAY");
+        }
 
-    _win = app->window;
-    _app = app;
+        if (eglInitialize(_display, 0, 0) != EGL_TRUE)
+        {
+            yourgame::loge("eglInitialize(..) returned with an error");
+        }
 
-    initInput(app);
-    initFileIO(app);
+        // use these attributes to choose a matching EGL frame buffer config
+        const EGLint eglAttribs[] = {
+            EGL_BLUE_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_RED_SIZE, 8,
+            EGL_DEPTH_SIZE, 24,
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_NONE};
 
-    // initialize logging
-    _context.logger = el::Loggers::getLogger("default");
-    el::Helpers::installLogDispatchCallback<elAndroidDispatcher>("AndroidDispatcher");
-    el::Helpers::uninstallLogDispatchCallback<el::base::DefaultLogDispatchCallback>("DefaultLogDispatchCallback");
-    elAndroidDispatcher *dispatcher = el::Helpers::logDispatchCallback<elAndroidDispatcher>("AndroidDispatcher");
-    dispatcher->setEnabled(true);
+        EGLint numConfigs = 0;
+        if (eglChooseConfig(_display, eglAttribs, nullptr, 0, &numConfigs) != EGL_TRUE)
+        {
+            yourgame::loge("eglChooseConfig(..) returned with an error");
+        }
 
-    // check the timer clock
-    yourgame::logi("timer clock is%v monotonic", _timer.isMonotonic() ? "" : " NOT");
+        if (numConfigs == 0)
+        {
+            yourgame::loge("eglChooseConfig(..) returned 0 matching configs");
+        }
 
-    // initialize EGL
-    _display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
-    if (_display == EGL_NO_DISPLAY)
-    {
-        yourgame::loge("eglGetDisplay(EGL_DEFAULT_DISPLAY) returned EGL_NO_DISPLAY");
-    }
+        /* the first returned config matches the desired attributes "best",
+        but might not be the ideal choice. we take in anyway.
+        todo: we could check attributes of the matching configs and choose
+        a suitable config by some criteria (with eglGetConfigAttrib(..)) */
+        yourgame::logi("choosing the first egl config (of %v total configs)", numConfigs);
 
-    if (eglInitialize(_display, 0, 0) != EGL_TRUE)
-    {
-        yourgame::loge("eglInitialize(..) returned with an error");
-    }
+        // get the actual (first) matching config
+        EGLConfig config;
+        eglChooseConfig(_display, eglAttribs, &config, 1, &numConfigs);
 
-    // use these attributes to choose a matching EGL frame buffer config
-    const EGLint eglAttribs[] = {
-        EGL_BLUE_SIZE, 8,
-        EGL_GREEN_SIZE, 8,
-        EGL_RED_SIZE, 8,
-        EGL_DEPTH_SIZE, 24,
-        EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-        EGL_NONE};
+        /* the attribute EGL_NATIVE_VISUAL_ID is an element of the enum AHardwareBuffer_Format,
+        that matches the chosen egl config. configure the buffers of the native window
+        appropriately (via ANativeWindow_setBuffersGeometry(..)) */
+        EGLint format;
+        eglGetConfigAttrib(_display, config, EGL_NATIVE_VISUAL_ID, &format);
+        yourgame::logi("calling ANativeWindow_setBuffersGeometry(..) with format %v", format);
+        ANativeWindow_setBuffersGeometry(_win, 0, 0, format);
 
-    EGLint numConfigs = 0;
-    if (eglChooseConfig(_display, eglAttribs, nullptr, 0, &numConfigs) != EGL_TRUE)
-    {
-        yourgame::loge("eglChooseConfig(..) returned with an error");
-    }
+        // create context, surface and make the context current
+        const EGLint contextAttribs[] = {
+            EGL_CONTEXT_CLIENT_VERSION, YOURGAME_GL_MAJOR,
+            EGL_NONE};
+        _eglContext = eglCreateContext(_display, config, EGL_NO_CONTEXT, contextAttribs);
+        if (_eglContext == EGL_NO_CONTEXT)
+        {
+            yourgame::loge("eglCreateContext(..) returned EGL_NO_CONTEXT");
+        }
+        _surface = eglCreateWindowSurface(_display, config, _win, NULL);
+        eglMakeCurrent(_display, _surface, _surface, _eglContext);
 
-    if (numConfigs == 0)
-    {
-        yourgame::loge("eglChooseConfig(..) returned 0 matching configs");
-    }
-
-    /* the first returned config matches the desired attributes "best",
-    but might not be the ideal choice. we take in anyway.
-    todo: we could check attributes of the matching configs and choose
-    a suitable config by some criteria (with eglGetConfigAttrib(..)) */
-    yourgame::logi("choosing the first egl config (of %v total configs)", numConfigs);
-
-    // get the actual (first) matching config
-    EGLConfig config;
-    eglChooseConfig(_display, eglAttribs, &config, 1, &numConfigs);
-
-    /* the attribute EGL_NATIVE_VISUAL_ID is an element of the enum AHardwareBuffer_Format,
-    that matches the chosen egl config. configure the buffers of the native window
-    appropriately (via ANativeWindow_setBuffersGeometry(..)) */
-    EGLint format;
-    eglGetConfigAttrib(_display, config, EGL_NATIVE_VISUAL_ID, &format);
-    yourgame::logi("calling ANativeWindow_setBuffersGeometry(..) with format %v", format);
-    ANativeWindow_setBuffersGeometry(_win, 0, 0, format);
-
-    // create context, surface and make the context current
-    const EGLint contextAttribs[] = {
-        EGL_CONTEXT_CLIENT_VERSION, YOURGAME_GL_MAJOR,
-        EGL_NONE};
-    _eglContext = eglCreateContext(_display, config, EGL_NO_CONTEXT, contextAttribs);
-    if (_eglContext == EGL_NO_CONTEXT)
-    {
-        yourgame::loge("eglCreateContext(..) returned EGL_NO_CONTEXT");
-    }
-    _surface = eglCreateWindowSurface(_display, config, _win, NULL);
-    eglMakeCurrent(_display, _surface, _surface, _eglContext);
-
-    // update window size in yourgame context
-    updateContextWinSize();
-
-    yourgame::logi("GL_VERSION: %v", glGetString(GL_VERSION));
-    yourgame::logi("GL_VENDOR: %v", glGetString(GL_VENDOR));
-    yourgame::logi("GL_RENDERER: %v", glGetString(GL_RENDERER));
-    yourgame::logi("GL_EXTENSIONS: %v", glGetString(GL_EXTENSIONS));
-
-#ifdef YOURGAME_EXTPROJ_imgui
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &imgio = ImGui::GetIO();
-    //imgio.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    //imgio.IniFilename = NULL;
-
-    ImGui::StyleColorsDark();
-    ImGui_ImplAndroid_Init(app);
-    ImGui_ImplOpenGL3_Init(YOURGAME_GLSL_VERSION_STRING);
-#endif
-
-    mygame::init(_context);
-
-    _initialized = true;
-}
-
-void tick()
-{
-    // update time delta in yourgame context
-    _context.deltaTimeUs = _timer.tick();
-    _context.deltaTimeS = ((double)_context.deltaTimeUs) * 1.0e-6;
-
-    if (_display != EGL_NO_DISPLAY)
-    {
         // update window size in yourgame context
         updateContextWinSize();
 
+        yourgame::logi("GL_VERSION: %v", glGetString(GL_VERSION));
+        yourgame::logi("GL_VENDOR: %v", glGetString(GL_VENDOR));
+        yourgame::logi("GL_RENDERER: %v", glGetString(GL_RENDERER));
+        yourgame::logi("GL_EXTENSIONS: %v", glGetString(GL_EXTENSIONS));
+
 #ifdef YOURGAME_EXTPROJ_imgui
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplAndroid_NewFrame();
-        ImGui::NewFrame();
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &imgio = ImGui::GetIO();
+        //imgio.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        //imgio.IniFilename = NULL;
+
+        ImGui::StyleColorsDark();
+        ImGui_ImplAndroid_Init(app);
+        ImGui_ImplOpenGL3_Init(YOURGAME_GLSL_VERSION_STRING);
 #endif
-        mygame::tick(_context);
-#ifdef YOURGAME_EXTPROJ_imgui
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-#endif
-        eglSwapBuffers(_display, _surface);
+
+        mygame::init(_context);
+
+        _initialized = true;
     }
-}
 
-void shutdown()
-{
-    if (!_initialized)
+    void tick()
     {
-        return;
-    }
+        // timing
+        auto now = std::chrono::steady_clock::now();
+        std::chrono::duration<double> duration = now - lastNowTime;
+        lastNowTime = now;
+        _context.deltaTimeS = (duration.count());
 
-#ifdef YOURGAME_EXTPROJ_imgui
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplAndroid_Shutdown();
-    ImGui::DestroyContext();
-#endif
-
-    if (_display != EGL_NO_DISPLAY)
-    {
-        eglMakeCurrent(_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-
-        if (_eglContext != EGL_NO_CONTEXT)
+        if (_display != EGL_NO_DISPLAY)
         {
-            eglDestroyContext(_display, _eglContext);
+            // update window size in yourgame context
+            updateContextWinSize();
+
+#ifdef YOURGAME_EXTPROJ_imgui
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplAndroid_NewFrame();
+            ImGui::NewFrame();
+#endif
+            mygame::tick(_context);
+#ifdef YOURGAME_EXTPROJ_imgui
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+#endif
+            eglSwapBuffers(_display, _surface);
+        }
+    }
+
+    void shutdown()
+    {
+        if (!_initialized)
+        {
+            return;
         }
 
-        if (_surface != EGL_NO_SURFACE)
+#ifdef YOURGAME_EXTPROJ_imgui
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplAndroid_Shutdown();
+        ImGui::DestroyContext();
+#endif
+
+        if (_display != EGL_NO_DISPLAY)
         {
-            eglDestroySurface(_display, _surface);
+            eglMakeCurrent(_display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+
+            if (_eglContext != EGL_NO_CONTEXT)
+            {
+                eglDestroyContext(_display, _eglContext);
+            }
+
+            if (_surface != EGL_NO_SURFACE)
+            {
+                eglDestroySurface(_display, _surface);
+            }
+
+            eglTerminate(_display);
         }
 
-        eglTerminate(_display);
+        _display = EGL_NO_DISPLAY;
+        _eglContext = EGL_NO_CONTEXT;
+        _surface = EGL_NO_SURFACE;
+
+        mygame::shutdown(_context);
+
+        _initialized = false;
     }
-
-    _display = EGL_NO_DISPLAY;
-    _eglContext = EGL_NO_CONTEXT;
-    _surface = EGL_NO_SURFACE;
-
-    mygame::shutdown(_context);
-
-    _initialized = false;
-}
 
 } // namespace yourgame
--- src/desktop_web/yourgame.cpp
+++ src/desktop_web/yourgame.cpp
@@ -17,12 +17,12 @@ freely, subject to the following restrictions:
    misrepresented as being the original software.
 3. This notice may not be removed or altered from any source distribution.
 */
+#include <chrono>
 #include "yourgame/gl_include.h"
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include "yourgame/yourgame.h"
 #include "yourgame_internal/yourgame_port.h"
-#include "yourgame_internal/timer.h"
 #include "yourgame_internal/mygame_external.h"
 
 #ifdef YOURGAME_EXTPROJ_imgui
@@ -36,160 +36,166 @@ INITIALIZE_EASYLOGGINGPP
 namespace yourgame
 {
 
-namespace
-{
-yourgame::context _context;
-yourgame_internal::Timer _timer(0U);
-bool _pendingShutdown = false;
-GLFWwindow *_window = NULL;
-void windowSizeCallback(GLFWwindow *window, int width, int height)
-{
-    _context.winWidth = width;
-    _context.winHeight = height;
-    _context.winAspectRatio = (float)width / (float)height;
-}
-} // namespace
-
-// API
-const yourgame::context &getCtx()
-{
-    return _context;
-}
-
-void notifyShutdown()
-{
-    _pendingShutdown = true;
-}
-
-int sendCmdToEnv(int cmdId, int data0, int data1, int data2)
-{
-    return -1;
-}
-
-// INTERNAL API
-bool pendingShutdown()
-{
-    return _pendingShutdown;
-}
-
-int init(int argc, char *argv[])
-{
-    // initialize logging
-    START_EASYLOGGINGPP(argc, argv);
-    _context.logger = el::Loggers::getLogger("default");
-
-    // initialize asset loading
-    initFileIO();
-
-    // check the timer clock
-    yourgame::logi("timer clock is%v monotonic", _timer.isMonotonic() ? "" : " NOT");
-
-    // initialize glfw, gl
-    yourgame::logi("glfwInit()...");
-    if (!glfwInit())
+    namespace
     {
-        yourgame::loge("glfwInit() failed");
+        yourgame::context _context;
+        std::chrono::steady_clock::time_point lastNowTime;
+        bool _pendingShutdown = false;
+
+        GLFWwindow *_window = NULL;
+        void windowSizeCallback(GLFWwindow *window, int width, int height)
+        {
+            _context.winWidth = width;
+            _context.winHeight = height;
+            _context.winAspectRatio = (float)width / (float)height;
+        }
+    } // namespace
+
+    // API
+    const yourgame::context &getCtx()
+    {
+        return _context;
+    }
+
+    void notifyShutdown()
+    {
+        _pendingShutdown = true;
+    }
+
+    int sendCmdToEnv(int cmdId, int data0, int data1, int data2)
+    {
         return -1;
     }
+
+    // INTERNAL API
+    bool pendingShutdown()
+    {
+        return _pendingShutdown;
+    }
+
+    int init(int argc, char *argv[])
+    {
+        // initialize logging
+        START_EASYLOGGINGPP(argc, argv);
+        _context.logger = el::Loggers::getLogger("default");
+
+        // initialize timing
+        lastNowTime = std::chrono::steady_clock::now();
+        double clockPeriod = (double)std::chrono::steady_clock::period::num /
+                             (double)std::chrono::steady_clock::period::den;
+        yourgame::logi("steady_clock precision: %vs (%vns)", clockPeriod, clockPeriod * 1.0e+9);
+
+        // initialize asset loading
+        initFileIO();
+
+        // initialize glfw, gl
+        yourgame::logi("glfwInit()...");
+        if (!glfwInit())
+        {
+            yourgame::loge("glfwInit() failed");
+            return -1;
+        }
 
 #if defined(YOURGAME_GL_API_GLES)
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 #elif defined(YOURGAME_GL_API_GL)
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 #endif
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, YOURGAME_GL_MAJOR);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, YOURGAME_GL_MINOR);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, YOURGAME_GL_MAJOR);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, YOURGAME_GL_MINOR);
 
-    _window = glfwCreateWindow(800, 512, "", NULL, NULL);
-    if (_window == NULL)
-    {
-        yourgame::loge("glfwCreateWindow() failed");
-        glfwTerminate();
-        return -1;
-    }
+        _window = glfwCreateWindow(800, 512, "", NULL, NULL);
+        if (_window == NULL)
+        {
+            yourgame::loge("glfwCreateWindow() failed");
+            glfwTerminate();
+            return -1;
+        }
 
-    glfwMakeContextCurrent(_window);
+        glfwMakeContextCurrent(_window);
 
-    // set window size callback
-    glfwSetWindowSizeCallback(_window, windowSizeCallback);
-    // call it manually to set initial size
-    int width, height;
-    glfwGetWindowSize(_window, &width, &height);
-    windowSizeCallback(_window, width, height);
+        // set window size callback
+        glfwSetWindowSizeCallback(_window, windowSizeCallback);
+        // call it manually to set initial size
+        int width, height;
+        glfwGetWindowSize(_window, &width, &height);
+        windowSizeCallback(_window, width, height);
 
 #if defined(YOURGAME_GL_EXT_LOADER_GLAD)
-    yourgame::logi("gladLoadGL()...");
-    if (!gladLoadGL())
-    {
-        yourgame::loge("gladLoadGL() failed");
-        return -1;
+        yourgame::logi("gladLoadGL()...");
+        if (!gladLoadGL())
+        {
+            yourgame::loge("gladLoadGL() failed");
+            return -1;
+        }
+#endif
+        yourgame::logi("GL_VERSION: %v", glGetString(GL_VERSION));
+        yourgame::logi("GL_VENDOR: %v", glGetString(GL_VENDOR));
+        yourgame::logi("GL_RENDERER: %v", glGetString(GL_RENDERER));
+        yourgame::logi("GL_SHADING_LANGUAGE_VERSION: %v", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+        glfwSwapInterval(1);
+        yourgame::initInput(_window);
+
+#ifdef YOURGAME_EXTPROJ_imgui
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &imgio = ImGui::GetIO();
+        imgio.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        imgio.IniFilename = NULL;
+
+        ImGui::StyleColorsDark();
+        ImGui_ImplGlfw_InitForOpenGL(_window, true);
+        ImGui_ImplOpenGL3_Init(YOURGAME_GLSL_VERSION_STRING);
+#endif
+
+        mygame::init(_context);
+        return 0;
     }
-#endif
-    yourgame::logi("GL_VERSION: %v", glGetString(GL_VERSION));
-    yourgame::logi("GL_VENDOR: %v", glGetString(GL_VENDOR));
-    yourgame::logi("GL_RENDERER: %v", glGetString(GL_RENDERER));
-    yourgame::logi("GL_SHADING_LANGUAGE_VERSION: %v", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
-    glfwSwapInterval(1);
-    yourgame::initInput(_window);
-
-#ifdef YOURGAME_EXTPROJ_imgui
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &imgio = ImGui::GetIO();
-    imgio.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    imgio.IniFilename = NULL;
-
-    ImGui::StyleColorsDark();
-    ImGui_ImplGlfw_InitForOpenGL(_window, true);
-    ImGui_ImplOpenGL3_Init(YOURGAME_GLSL_VERSION_STRING);
-#endif
-
-    mygame::init(_context);
-    return 0;
-}
-
-void tick()
-{
-    glfwPollEvents();
-
-    // update context
-    _context.deltaTimeUs = _timer.tick();
-    _context.deltaTimeS = ((double)_context.deltaTimeUs) * 1.0e-6;
-
-#ifdef YOURGAME_EXTPROJ_imgui
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-#endif
-
-    mygame::tick(_context);
-
-#ifdef YOURGAME_EXTPROJ_imgui
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-#endif
-
-    glfwSwapBuffers(_window);
-}
-
-int shutdown()
-{
-#ifdef YOURGAME_EXTPROJ_imgui
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-#endif
-
-    if (_window != NULL)
+    void tick()
     {
-        glfwDestroyWindow(_window);
+        glfwPollEvents();
+
+        // timing
+        auto now = std::chrono::steady_clock::now();
+        std::chrono::duration<double> duration = now - lastNowTime;
+        lastNowTime = now;
+        _context.deltaTimeS = (duration.count());
+
+#ifdef YOURGAME_EXTPROJ_imgui
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+#endif
+
+        mygame::tick(_context);
+
+#ifdef YOURGAME_EXTPROJ_imgui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+#endif
+
+        glfwSwapBuffers(_window);
     }
-    glfwTerminate();
 
-    mygame::shutdown(_context);
+    int shutdown()
+    {
+#ifdef YOURGAME_EXTPROJ_imgui
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+#endif
 
-    return 0;
-}
+        if (_window != NULL)
+        {
+            glfwDestroyWindow(_window);
+        }
+        glfwTerminate();
+
+        mygame::shutdown(_context);
+
+        return 0;
+    }
 
 } // namespace yourgame
