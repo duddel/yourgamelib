332723e83be4bcc50376815bb69fa19a99b97000
duddel <duddel@users.noreply.github.com>
2021-01-18 13:18:26 +0000

    Deploying to gh-pages from  @ 8e957137f2445340a88d053f6198299a4b63cd62 🚀

--- search/variables_8.js
+++ search/variables_8.js
@@ -2,9 +2,11 @@ var searchData=
 [
   ['umax',['uMax',['../structyourgame_1_1GLTextureAtlas_1_1Coords.html#a8bd960f5eb7cbbf3c9f5b8e8337dfdbb',1,'yourgame::GLTextureAtlas::Coords']]],
   ['umin',['uMin',['../structyourgame_1_1GLTextureAtlas_1_1Coords.html#ae38923891f6b03bd86d5d9f49ae54e76',1,'yourgame::GLTextureAtlas::Coords']]],
+  ['unifnamecameraposition',['unifNameCameraPosition',['../namespaceyourgame.html#ae1db8789c09ec427fa24a00e762c66a8',1,'yourgame']]],
   ['unifnamemodelmatrix',['unifNameModelMatrix',['../namespaceyourgame.html#a279c209c0420169510bae3a3e56fc0b7',1,'yourgame']]],
   ['unifnamemvpmatrix',['unifNameMvpMatrix',['../namespaceyourgame.html#a9d277cc2294828592d34e13fde329983',1,'yourgame']]],
   ['unifnamenormalmatrix',['unifNameNormalMatrix',['../namespaceyourgame.html#a3bf2c1e650e39fe4c2ffb6016fbb063e',1,'yourgame']]],
+  ['unifnameskyrotationinv',['unifNameSkyRotationInv',['../namespaceyourgame.html#a11d5556309923a4b73be46e8576547f1',1,'yourgame']]],
   ['unifnametexturediffuse',['unifNameTextureDiffuse',['../namespaceyourgame.html#acaaf7d5562c8fd69853bc7775ca62963',1,'yourgame']]],
   ['unifnametexturesky',['unifNameTextureSky',['../namespaceyourgame.html#ad4e9aa889f4404e7c6717853d9a65b62',1,'yourgame']]],
   ['unifvaluetexturediffuse',['unifValueTextureDiffuse',['../namespaceyourgame.html#a6bdef5ba2d62f0174350cbae7d9f0da4',1,'yourgame']]],
