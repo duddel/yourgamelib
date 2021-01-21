15ac435da0421d521086497cdfa4e205e05fb78f
duddel <duddel@users.noreply.github.com>
2021-01-21 23:57:56 +0100

    integrate bullet engine

--- examples/framework/init.bash
+++ examples/framework/init.bash
@@ -3,4 +3,4 @@
 cd "${0%/*}"
 cd ..
 
-python3 ../init.py framework --noSources --extProj box2d flecs lua choreograph fsm qu3e
+python3 ../init.py framework --noSources --extProj box2d flecs lua choreograph fsm qu3e bullet
--- examples/framework/init.cmd
+++ examples/framework/init.cmd
@@ -3,4 +3,4 @@
 cd %~dp0
 cd ..
 
-python ../init.py framework --noSources --extProj box2d flecs lua choreograph fsm qu3e
+python ../init.py framework --noSources --extProj box2d flecs lua choreograph fsm qu3e bullet
