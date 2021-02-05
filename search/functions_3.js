244749fbac5f90ea4b9e9bb578e843774aa5f3e8
duddel <duddel@users.noreply.github.com>
2021-02-05 20:23:51 +0000

    Deploying to gh-pages from  @ 5692e60c5d4579972ea228feeb408220fa1e24f0 🚀

--- search/functions_3.js
+++ search/functions_3.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['destroy',['destroy',['../classyourgame_1_1AssetManager.html#af93e6b1eda81646b607c052d1422fd16',1,'yourgame::AssetManager::destroy(std::string name)'],['../classyourgame_1_1AssetManager.html#a57b79cb5457c9811fae31ca6a8b2d268',1,'yourgame::AssetManager::destroy()']]],
   ['draw',['draw',['../classyourgame_1_1GLShape.html#a2f7a98fb105da31a0507a5234fc4e1a8',1,'yourgame::GLShape']]],
-  ['drawall',['drawAll',['../classyourgame_1_1GLGeometry.html#aec600327a4d90ba83af644368b6ce0d7',1,'yourgame::GLGeometry']]]
+  ['drawall',['drawAll',['../classyourgame_1_1GLGeometry.html#aa38597be88dd8e705034a4ca7a276543',1,'yourgame::GLGeometry']]]
 ];
