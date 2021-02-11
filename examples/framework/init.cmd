7a2af48b2125114904f4d4b30cd5829aa9c7cc8b
duddel <duddel@users.noreply.github.com>
2021-02-11 22:22:38 +0100

    remove unused dep (bullet) from framework example

--- examples/framework/init.cmd
+++ examples/framework/init.cmd
@@ -3,4 +3,4 @@
 cd %~dp0
 cd ..
 
-python ../init.py framework --noStub --extProj box2d flecs lua choreograph fsm qu3e bullet
+python ../init.py framework --noStub --extProj box2d flecs lua choreograph fsm qu3e
