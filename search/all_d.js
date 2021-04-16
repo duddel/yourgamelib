f4582e64e8da4303e374abdb315de94e4906a678
duddel <duddel@users.noreply.github.com>
2021-04-16 20:43:42 +0000

    Deploying to gh-pages from  @ 9c71e4b026fc9c36f7fae6e693f24a8cc46c0680 🚀

--- search/all_d.js
+++ search/all_d.js
@@ -4,7 +4,7 @@ var searchData=
   ['particles',['Particles',['../classyourgame_1_1Particles.html',1,'yourgame::Particles'],['../classyourgame_1_1Particles.html#a89efd03ff0b14154f5a855c2d9919a99',1,'yourgame::Particles::Particles()']]],
   ['particles_2eh',['particles.h',['../particles_8h.html',1,'']]],
   ['perspective',['PERSPECTIVE',['../classyourgame_1_1Camera.html#abd9deece2ac9cf689efde21234a20ee2ad46c97be63d6c4cb887419a4a3df5347',1,'yourgame::Camera']]],
-  ['pmat',['pMat',['../classyourgame_1_1Camera.html#a20cb4c6b70d47328f11180fbe3956a5d',1,'yourgame::Camera']]],
+  ['pmat',['pMat',['../classyourgame_1_1Camera.html#a20cb4c6b70d47328f11180fbe3956a5d',1,'yourgame::Camera::pMat()'],['../classyourgame_1_1Camera.html#abc06ddbf26079439a88fa7e5f90fc4fc',1,'yourgame::Camera::pMat(float zNear, float zFar)']]],
   ['pointer',['pointer',['../structyourgame_1_1GLShape_1_1ArrBufferDescr.html#aab2fd7f350b28de0a91e5724f63c5cba',1,'yourgame::GLShape::ArrBufferDescr']]],
   ['pointto',['pointTo',['../classyourgame_1_1Trafo.html#a8456ea47403bbad5f40b5c11633bd56e',1,'yourgame::Trafo']]],
   ['position',['position',['../classyourgame_1_1GLLightsource.html#a480cc8e1e1e4a3e28254b7ba1ea284ef',1,'yourgame::GLLightsource']]],
--- search/functions_c.js
+++ search/functions_c.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['particles',['Particles',['../classyourgame_1_1Particles.html#a89efd03ff0b14154f5a855c2d9919a99',1,'yourgame::Particles']]],
-  ['pmat',['pMat',['../classyourgame_1_1Camera.html#a20cb4c6b70d47328f11180fbe3956a5d',1,'yourgame::Camera']]],
+  ['pmat',['pMat',['../classyourgame_1_1Camera.html#a20cb4c6b70d47328f11180fbe3956a5d',1,'yourgame::Camera::pMat()'],['../classyourgame_1_1Camera.html#abc06ddbf26079439a88fa7e5f90fc4fc',1,'yourgame::Camera::pMat(float zNear, float zFar)']]],
   ['pointto',['pointTo',['../classyourgame_1_1Trafo.html#a8456ea47403bbad5f40b5c11633bd56e',1,'yourgame::Trafo']]],
   ['position',['position',['../classyourgame_1_1GLLightsource.html#a480cc8e1e1e4a3e28254b7ba1ea284ef',1,'yourgame::GLLightsource']]],
   ['pushcoords',['pushCoords',['../classyourgame_1_1GLTextureAtlas.html#a9221ab669068499351739df6d598471b',1,'yourgame::GLTextureAtlas']]],
