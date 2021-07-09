3d3e673a3f4c2b92399c06ad86b96ef15fba75a1
duddel <duddel@users.noreply.github.com>
2021-07-09 16:21:19 +0000

    Deploying to gh-pages from  @ bef99072705020f3439df4ac7e957b2090c24efe 🚀

--- search/all_9.js
+++ search/all_9.js
@@ -7,11 +7,11 @@ var searchData=
   ['loadtexture',['loadTexture',['../namespaceyourgame.html#a37bd4325743373e8c3b29d0667c9eafe',1,'yourgame']]],
   ['loadtextureatlascrunch',['loadTextureAtlasCrunch',['../namespaceyourgame.html#a90e847f8ffdaac6347e0d295123a3066',1,'yourgame']]],
   ['loadtextureatlasgrid',['loadTextureAtlasGrid',['../namespaceyourgame.html#a43de560a330f967d1313bb6493d1a10b',1,'yourgame']]],
-  ['logd',['logd',['../namespaceyourgame.html#aa018299735de2685349d3bfdbc86645b',1,'yourgame::logd(const char *s, const T &amp;value, const Args &amp;... args)'],['../namespaceyourgame.html#a5e70120e1d5ceb992552dbcaaff069cd',1,'yourgame::logd(const T &amp;value)']]],
-  ['loge',['loge',['../namespaceyourgame.html#a389e47f66fb1ab81f132fd48c0fa73cb',1,'yourgame::loge(const char *s, const T &amp;value, const Args &amp;... args)'],['../namespaceyourgame.html#a1249e887a2994cd7aaf6ad968f5ccd49',1,'yourgame::loge(const T &amp;value)']]],
+  ['logd',['logd',['../namespaceyourgame.html#ab2d9ea56dbffd7ce41fc101dda08cf0a',1,'yourgame::logd(const char *s, const T &amp;value, const Args &amp;...args)'],['../namespaceyourgame.html#a5e70120e1d5ceb992552dbcaaff069cd',1,'yourgame::logd(const T &amp;value)']]],
+  ['loge',['loge',['../namespaceyourgame.html#a7785752152f0a6ae09d2952a2c6b1c85',1,'yourgame::loge(const char *s, const T &amp;value, const Args &amp;...args)'],['../namespaceyourgame.html#a1249e887a2994cd7aaf6ad968f5ccd49',1,'yourgame::loge(const T &amp;value)']]],
   ['logging_2eh',['logging.h',['../logging_8h.html',1,'']]],
-  ['logi',['logi',['../namespaceyourgame.html#a0f67421bf076411d33636b98d6a6a84a',1,'yourgame::logi(const char *s, const T &amp;value, const Args &amp;... args)'],['../namespaceyourgame.html#a3acfaf5bf5d8db218fdf3ec6138c449c',1,'yourgame::logi(const T &amp;value)']]],
-  ['logw',['logw',['../namespaceyourgame.html#acbaf01185bb8023afd3920fa615a5fb5',1,'yourgame::logw(const char *s, const T &amp;value, const Args &amp;... args)'],['../namespaceyourgame.html#ab1428256b7d8aa6c99ad601f780f9f08',1,'yourgame::logw(const T &amp;value)']]],
+  ['logi',['logi',['../namespaceyourgame.html#ac150a5fd361f821b3de5adb7e9488e74',1,'yourgame::logi(const char *s, const T &amp;value, const Args &amp;...args)'],['../namespaceyourgame.html#a3acfaf5bf5d8db218fdf3ec6138c449c',1,'yourgame::logi(const T &amp;value)']]],
+  ['logw',['logw',['../namespaceyourgame.html#a9c913c067e86bfb42ef1718a3f9c152d',1,'yourgame::logw(const char *s, const T &amp;value, const Args &amp;...args)'],['../namespaceyourgame.html#ab1428256b7d8aa6c99ad601f780f9f08',1,'yourgame::logw(const T &amp;value)']]],
   ['lookat',['lookAt',['../classyourgame_1_1Trafo.html#ac75cc92de811ebe7e6987c1a80ec3792',1,'yourgame::Trafo']]],
   ['ls',['ls',['../namespaceyourgame.html#a6f4ca4b7b4d4f7eba9332ed57db24ac2',1,'yourgame']]]
 ];
